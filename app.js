1a76bb76261ad56d9728a8b74fb40428d33c718b
omega1984 <omegareborn2019@gmail.com>
2019-07-29 15:04:18 -0700

    solo

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // `state` is just an object literal
+    this.state = {
+      done: false
+    };
+  }
+
+  // When a list item is clicked, we will toggle the `done`
+  // boolean, and our component's `render` method will run again
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    // Making the style conditional on our `state` lets us
+    // update it based on user interactions.
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    // You can pass inline styles using React's `style` attribute to any component
+    // snake-cased css properties become camelCased this this object
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']} /> // Here we are creating an instance of the `TodoList` component
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
